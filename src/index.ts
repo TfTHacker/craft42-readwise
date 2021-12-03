fe48262d796b9cca627d0b2473d27a4bde9157b6
TfTHacker <69121180+TfTHacker@users.noreply.github.com>
2021-12-03 21:44:40 +0100

    error handling for lack of acces token

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ let bookListDiv: HTMLDivElement;
 let buttonConfigureReadwise: HTMLImageElement;
 let divSettingsWrapper: HTMLDivElement;
 
-
 window.addEventListener("load", async () => {
   btnRefreshHighlightList = <HTMLButtonElement> document.getElementById('btn-execute');
   inputReadwiseApiToken = <HTMLInputElement> document.getElementById("readwise-api-key")
   bookListDiv = <HTMLDivElement> document.getElementById("book-list");
   buttonConfigureReadwise = <HTMLImageElement> document.getElementById("btn-configure-readwise");
   divSettingsWrapper = <HTMLDivElement> document.getElementById("config-readwise-setings");
+
   // prepare event handlers
   btnRefreshHighlightList?.addEventListener("click", async () => {
       bookListDiv.innerHTML="";
@@ -31,19 +31,25 @@ window.addEventListener("load", async () => {
   buttonConfigureReadwise?.addEventListener("click", async () => {
     divSettingsWrapper.style.display = divSettingsWrapper.style.display==="" ? "inline" : "";
     bookListDiv.innerHTML = "";
+    bookListDiv.style.height="0px";
+    btnRefreshHighlightList.style.visibility="hidden";
     if(inputReadwiseApiToken.value.trim()!="") btnRefreshHighlightList.style.display="inline";
-    if(divSettingsWrapper.style.display!="inline") await listBooks();
+    if(divSettingsWrapper.style.display==="") {
+      bookListDiv.innerHTML = "";
+      bookListDiv.style.height="450px";
+      btnRefreshHighlightList.style.visibility="visible";
+    }
   });
 
   // initialize UI
   const rwToken = await craft.storageApi.get("readwiseToken");
-  console.log("rwToken", rwToken);
   if (rwToken.status != "error" && rwToken.data != "") {
       inputReadwiseApiToken.value = rwToken.data;
       btnRefreshHighlightList.style.display = "inline";
       await listBooks();
   } else {
     divSettingsWrapper.style.display = "inline";
+    bookListDiv.style.height="0px";
   }
 
 })
@@ -55,7 +61,6 @@ const insertHighlights = async (id : string) => {
   craft.dataApi.addBlocks( [ craft.blockFactory.horizontalLineBlock({lineStyle:"light"}) ]);
   const bulletStyle = craft.blockFactory.defaultListStyle("bullet");
   const allHighlights = highlights.results.map( (h:any) => {
-    console.log(h)
     return craft.blockFactory.textBlock({
       listStyle: bulletStyle,
       content: [
@@ -72,18 +77,23 @@ const listBooks = async () => {
   const rwToken = await craft.storageApi.get("readwiseToken");
   const bookList = await readwiseGetBookList(<string>rwToken.data)
   let output = "";
+  if(bookList===null) {
+    bookListDiv.innerHTML="Information could not be retrieved from Readwise. Please verify the Readwise Access Token."
+    return;
+  }
   bookList.forEach((e : any) => {
     if(e.num_highlights===0) return;
-    output += `<div class="ReadWiseBook" id="${e.id}" style="width=250px; display: flex; border-top-style:dashed; border-top-width:1px; padding-top:5px">
-              <span style="width:50px"><img src="${e.cover_image_url}" width="45px"></span>
-              <span style="width:195px;padding-left:5px"">
+    output += `<div class="ReadWiseBook"  style="padding-bottom:4px; width=250px; display: flex; border-top-style:dashed; border-top-width:1px; padding-top:5px">
+            <span style="width:50px;padding-left:5px;"><img src="${e.cover_image_url}" width="45px"></span>
+              <span style="width:170px;padding-left:5px"">
                 <div >${e.title} (${e.num_highlights})</div>
                 <div>${e.author}</div>
-              </span>                  
+              </span>
+              <span><img class="btn-insert-highlights" id="${e.id}" src="https://readwise-assets.s3.amazonaws.com/static/images/new_icons/import.30df72e7b737.svg"></span>                 
             </div>`;
   });
   bookListDiv.innerHTML = output;
-  document.querySelectorAll(".ReadWiseBook").forEach(async (i) => {
+  document.querySelectorAll(".btn-insert-highlights").forEach(async (i) => {
       i.addEventListener("click", async (e) => await insertHighlights(i.id) );
   });
 }
--- src/readwiseStuff.ts
+++ src/readwiseStuff.ts
@@ -1,15 +1,19 @@
 export const readwiseGetBookList = async (rwToken : string) => {
-    const bookList = await fetch('https://readwise.io/api/v2/books/?page_size=1000', {
-        headers: {
-            "Authorization": "Token " + rwToken
-        }
-    });
-    // sort by date, newest to oldest
-    return (await bookList.json()).results.sort( (a:any,b:any)=>{
-        const c = new Date(a.last_highlight_at).getTime();
-        const d = new Date(b.last_highlight_at).getTime();
-        return d - c;
-    });
+    try {
+        const bookList = await fetch('https://readwise.io/api/v2/books/?page_size=1000', {
+            headers: {
+                "Authorization": "Token " + rwToken
+            }
+        })
+        // sort by date, newest to oldest
+        return (await bookList.json()).results.sort( (a:any,b:any)=>{
+            const c = new Date(a.last_highlight_at).getTime();
+            const d = new Date(b.last_highlight_at).getTime();
+            return d - c;
+        });            
+    } catch (error) {
+        return null;
+    }
 }
 
 
